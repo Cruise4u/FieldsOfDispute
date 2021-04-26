0cf2e8ce0b4b5810f106bcc9606df93d17f50eff
Cruise4u <pedrofrancisco90@gmail.com>
2021-04-26 17:25:56 +0200

    Started Grid creation and camera view over grid

---
+++ PocketArenaGame/Assets/Resources/Code/FieldNodeData.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu]
+public class FieldNodeData : ScriptableObject
+{
+    public GameObject nodeVisualizationGO;
+    public float width;
+    public Material playableMaterial;
+    public Material nonplayableMaterial;
+}
