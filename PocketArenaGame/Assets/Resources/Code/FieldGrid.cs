d3c4e41022e0c803f5a5938bd69085e2ba6b88b8
Cruise4u <pedrofrancisco90@gmail.com>
2021-04-26 13:34:11 +0200

    Added Plugins (Dotween/Bolt/External Assets)
    Merging to master and starting from there a new branch from which new features will be developed upon.

---
+++ PocketArenaGame/Assets/Resources/Code/FieldGrid.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using UnityEngine;
+
+public class GridMain
+{
+    public int xSize;
+    public int ySize;
+    public static GridNode[,] nodes;
+
+    public void CreateGrid()
+    {
+        nodes = new GridNode[xSize, ySize];
+        for (int y = 0; y < ySize; y++)
+        {
+
+            for (int x = 0; x < xSize; x++)
+            {
+                nodes[x, y] = new GridNode(x, y);
+
+            }
+        }
+
+    }
+
+
+
+
+
+}
+
+public class GridNode
+{
+    public GameObject unitStationed;
+    private int xPos;
+    private int yPos;
+
+    public GridNode(int xPos, int yPos)
+    {
+        this.xPos = xPos;
+        this.yPos = yPos;
+    }
+
+
+
+
+
+
+}
